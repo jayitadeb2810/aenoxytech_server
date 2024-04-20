5d3b94889ff442b3075e9a4b49933ddc09ae63b5
Jayita Deb <jayitadeb2810@gmail.com>
2024-04-20 14:55:18 +0530

    new commit

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
-import dotenv from "dotenv"
-import cloudinary from "cloudinary"
-import app from "./app.js"
-import { connectDatabase } from "./config/database.js"
+import express from "express"
+const app = express()
 
-dotenv.config()
-
-// const options = {
-//   origin: "http://localhost:3000",
-//   methods: "GET, PUT, POST, DELETE",
-// }
-
-// app.use(cors(options))
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+app.get("/", (req, res) => {
+  res.send("Hello World!")
 })
 
-app.listen(process.env.PORT, () => {
-  console.log(
-    `Server running at http://localhost:${process.env.PORT}`
-  )
+app.listen(port, () => {
+  console.log(`Example app listening on port `)
 })
 
-connectDatabase()
+// import dotenv from "dotenv"
+// import cloudinary from "cloudinary"
+// import app from "./app.js"
+// import { connectDatabase } from "./config/database.js"
+
+// dotenv.config()
+
+// // const options = {
+// //   origin: "http://localhost:3000",
+// //   methods: "GET, PUT, POST, DELETE",
+// // }
+
+// // app.use(cors(options))
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// })
+
+// app.listen(process.env.PORT, () => {
+//   console.log(
+//     `Server running at http://localhost:${process.env.PORT}`
+//   )
+// })
+
+// connectDatabase()
