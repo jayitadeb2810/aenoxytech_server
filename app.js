02e2b8aaabb87ac67ec8de024164e98adad0f3e1
Jayita Deb <jayitadeb2810@gmail.com>
2024-04-20 17:25:34 +0530

    new commit3

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const app = express()
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://aenoxytech-application.onrender.com/"
-    // "http://localhost:3000"
+    "http://localhost:3000"
   )
   res.setHeader(
     "Access-Control-Allow-Methods",
